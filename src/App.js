64777af0e6276207c283b135fa670414743f1cf9
spig0126 <spig0126@gmail.com>
2022-02-06 17:27:52 +0900

    Feat: display homepage content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
+import Navigation from "./components/Navigation";
+import MainPage from "./pages/MainPage/MainPage";
+import Bottom from "./components/Bottom";
+
 function App() {
   return (
     <>
-      <Link to="/mypage/sellbuyrecord">SellBuyRecord</Link>
-      <Link to="/mypage/region">SelectRegion</Link>
+      <Navigation />
+      <MainPage />
+      <Bottom />
     </>
   );
 }
