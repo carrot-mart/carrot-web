90d3acfc123a31a742cf055e79fea169c537c9f3
SeungHyeok Myeong <audtmdgur@naver.com>
2022-02-06 16:47:33 +0900

    Initial settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import SignPage from "./pages/SignPage";
+// import SelectRegion from "./components/MyPage/SelectRegion/SelectRegion";
+
+import Navigation from "./components/Navigation"
+import Bottom from "./components/Bottom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+
+    <Bottom />
+           
+
+	);
 }
 
 export default App;
