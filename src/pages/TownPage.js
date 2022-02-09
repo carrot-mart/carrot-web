3d7dfda80b544289de996023f75b3fc5beb58bb2
spig0126 <spig0126@gmail.com>
2022-02-10 00:01:55 +0900

    Design: synchronize layout

--- src/pages/TownPage.js
+++ src/pages/TownPage.js
@@ -1,51 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
 
-import TownInfoBox from '../components/Town/TownInfoBox'
-import TownStoreBox from '../components/Town/TownStoreBox'
-import TownPostscript from '../components/Town/TownPostscript'
-
-
+import TownInfoBox from "../components/Town/TownInfoBox";
+import TownStoreBox from "../components/Town/TownStoreBox";
+import TownPostscript from "../components/Town/TownPostscript";
 
 // 승혁
 import Navigation from "../components/Navigation";
 import Bottom from "../components/Bottom";
 
-
 function TownMainPage() {
-
-    return (
-
-        <TownMainContainer>
-            <TownMainBox>
-                <Navigation />
-                <TownInfoBox />
-                <TownStoreBox />
-                <TownPostscript />
-                <Bottom />
-            </TownMainBox>
-
-        </TownMainContainer>
-
-    );
+  return (
+    <TownMainContainer>
+      <TownMainBox>
+        <Navigation />
+        <TownInfoBox />
+        <TownStoreBox />
+        <TownPostscript />
+        <Bottom />
+      </TownMainBox>
+    </TownMainContainer>
+  );
 }
-
 
 export default TownMainPage;
 
-
 const TownMainContainer = styled.div`
-    margin : 0 auto;
-    width: 100%;
-    hegiht:fit-content;
-    min-height: 5rem;
+  margin: 0 auto;
+  width: 100%;
+  height: fit-content;
+  min-height: 5rem;
 `;
 
 const TownMainBox = styled.div`
-    margin : 0 auto;
-    width : 100%;
-    hegiht:fit-content;
-    min-height: 5rem;
+  margin: 0 auto;
+  width: 100%;
+  height: fit-content;
+  min-height: 5rem;
 `;
-
-
