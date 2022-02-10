72f0107a31a24e79af376a6dc266a0eaaec710d6
spig0126 <spig0126@gmail.com>
2022-02-10 14:33:04 +0900

    Feat: add loginReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import likesReducer from "./features/likes/likesSlice";
+import loginReducer from "./features/login/loginSlice";
 
-export default configureStore({ reducer: { likes: likesReducer } });
+export default configureStore({
+  reducer: { likes: likesReducer, login: loginReducer },
+});
