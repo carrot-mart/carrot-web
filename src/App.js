18a9e7b024821c968d70cc96449a5b210a15464b
spig0126 <spig0126@gmail.com>
2022-02-10 18:41:16 +0900

    Refractor: load main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 import "./globalStyles.css";
 import TownMainPage from "./pages/TownPage";
 import LikeRecordPage from "./pages/MyPage/LikeRecordPage";
 import { Link } from "react-router-dom";
 import SearchResultPage from "./pages/MainPage/SearchResultPage";
+import MainPage from "./pages/MainPage/MainPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <Link to="/mypage/likerecord">Like Record</Link>
-      <Link to="/mypage/sellbuyrecord">SellBuyRecird</Link>
-      <SearchResultPage />
+      <Navigation />
+      <MainPage />
     </div>
   );
 }
